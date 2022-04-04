eabec6bd5d54ae3f24c42922a80bd2faf635b8b7
borodedamie <opeoluborode@yahoo.com>
2022-04-04 04:04:25 -0500

    removed router to use django's urlpatterns

--- schedulingapp/views.py
+++ schedulingapp/views.py
@@ -5,45 +5,136 @@ from schedulingapp.serializers import CompanySerializer, BranchSerializer, Depar
 from rest_framework import generics, permissions, viewsets
 from rest_framework.decorators import action
 
-# Create your views here.
-class CompanyViewSet(viewsets.ModelViewSet):
+class CompanyList(generics.ListCreateAPIView):
     queryset = Company.objects.all()
-    permission_classes = [
-        permissions.IsAuthenticated
-    ]
     serializer_class = CompanySerializer
+    
+    permission_classes = [ permissions.IsAuthenticated ]
 
-#branch  
-class BranchViewSet(viewsets.ModelViewSet):
+
+class CompanyDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+    
+    permission_classes = [ permissions.IsAuthenticated ]
+    
+class BranchList(generics.ListCreateAPIView):
     queryset = Branch.objects.all()
-    permission_classes = [
-        permissions.IsAuthenticated
-    ]
     serializer_class = BranchSerializer
+    
+    permission_classes = [ permissions.IsAuthenticated ]
 
-# department  
-class DepartmentViewSet(viewsets.ModelViewSet):
+
+class BranchDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Branch.objects.all()
+    serializer_class = BranchSerializer
+    
+    permission_classes = [ permissions.IsAuthenticated ]
+    
+class DepartmentList(generics.ListCreateAPIView):
     queryset = Department.objects.all()
-    permission_classes = [
-        permissions.IsAuthenticated
-    ]
     serializer_class = DepartmentSerializer
+    
+    permission_classes = [ permissions.IsAuthenticated ]
 
-# employee   
-class EmployeeViewSet(viewsets.ModelViewSet):
+
+class DepartmentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+    
+    permission_classes = [ permissions.IsAuthenticated ]
+    
+class EmployeeList(generics.ListCreateAPIView):
     queryset = Employee.objects.all()
-    permission_classes = [
-        permissions.IsAuthenticated
-    ]
     serializer_class = EmployeeSerializer
     
-# client   
-class ClientViewSet(viewsets.ModelViewSet):
+    permission_classes = [ permissions.IsAuthenticated ]
+
+
+class EmployeeDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    
+    permission_classes = [ permissions.IsAuthenticated ]
+    
+class ClientList(generics.ListCreateAPIView):
     queryset = Client.objects.all()
-    permission_classes = [
-        permissions.IsAuthenticated
-    ]
     serializer_class = ClientSerializer
+    
+    permission_classes = [ permissions.IsAuthenticated ]
+
+
+class ClientDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+    
+    permission_classes = [ permissions.IsAuthenticated ]
+    
+class GroupList(generics.ListCreateAPIView):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    
+    permission_classes = [ permissions.IsAuthenticated ]
+
+
+class GroupDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    
+    permission_classes = [ permissions.IsAuthenticated ]
+    
+class EventList(generics.ListCreateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    
+    permission_classes = [ permissions.IsAuthenticated ]
+
+
+class EventDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    
+    permission_classes = [ permissions.IsAuthenticated ]
+    
+# Create your views here.
+# class CompanyViewSet(viewsets.ModelViewSet):
+#     queryset = Company.objects.all()
+#     permission_classes = [
+#         permissions.IsAuthenticated
+#     ]
+#     serializer_class = CompanySerializer
+
+#branch  
+# class BranchViewSet(viewsets.ModelViewSet):
+#     queryset = Branch.objects.all()
+#     permission_classes = [
+#         permissions.IsAuthenticated
+#     ]
+#     serializer_class = BranchSerializer
+
+# department  
+# class DepartmentViewSet(viewsets.ModelViewSet):
+#     queryset = Department.objects.all()
+#     permission_classes = [
+#         permissions.IsAuthenticated
+#     ]
+#     serializer_class = DepartmentSerializer
+
+# # employee   
+# class EmployeeViewSet(viewsets.ModelViewSet):
+#     queryset = Employee.objects.all()
+#     permission_classes = [
+#         permissions.IsAuthenticated
+#     ]
+#     serializer_class = EmployeeSerializer
+    
+# # client   
+# class ClientViewSet(viewsets.ModelViewSet):
+#     queryset = Client.objects.all()
+#     permission_classes = [
+#         permissions.IsAuthenticated
+#     ]
+#     serializer_class = ClientSerializer
 
 # class UserViewSet(viewsets.ModelViewSet):
 #     queryset = User.objects.all()
@@ -87,18 +178,18 @@ class CreateAdminUser(generics.CreateAPIView):
         instance.save()
     
 # groups 
-class GroupViewSet(viewsets.ModelViewSet):
-    queryset = Group.objects.all()
-    permission_classes = [
-        permissions.IsAuthenticated
-    ]
-    serializer_class = GroupSerializer
+# class GroupViewSet(viewsets.ModelViewSet):
+#     queryset = Group.objects.all()
+#     permission_classes = [
+#         permissions.IsAuthenticated
+#     ]
+#     serializer_class = GroupSerializer
     
 # events    
-class EventViewSet(viewsets.ModelViewSet):
-    queryset = Event.objects.all()
-    permission_classes = [
-        permissions.IsAuthenticated
-    ]
-    serializer_class = EventSerializer
+# class EventViewSet(viewsets.ModelViewSet):
+#     queryset = Event.objects.all()
+#     permission_classes = [
+#         permissions.IsAuthenticated
+#     ]
+#     serializer_class = EventSerializer
     
