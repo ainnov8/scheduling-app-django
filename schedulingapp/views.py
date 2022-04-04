e4a089ad1b538b191f34dec4f239bd5ab446dcab
borodedamie <opeoluborode@yahoo.com>
2022-04-04 04:51:21 -0500

    remigrated DB

--- schedulingapp/views.py
+++ schedulingapp/views.py
@@ -5,147 +5,147 @@ from schedulingapp.serializers import CompanySerializer, BranchSerializer, Depar
 from rest_framework import generics, permissions, viewsets
 from rest_framework.decorators import action
 
-class CompanyList(generics.ListCreateAPIView):
-    queryset = Company.objects.all()
-    serializer_class = CompanySerializer
-    
-    permission_classes = [ permissions.IsAuthenticated ]
-
-
-class CompanyDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Company.objects.all()
-    serializer_class = CompanySerializer
-    
-    permission_classes = [ permissions.IsAuthenticated ]
-    
-class BranchList(generics.ListCreateAPIView):
-    queryset = Branch.objects.all()
-    serializer_class = BranchSerializer
-    
-    permission_classes = [ permissions.IsAuthenticated ]
-
-
-class BranchDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Branch.objects.all()
-    serializer_class = BranchSerializer
-    
-    permission_classes = [ permissions.IsAuthenticated ]
-    
-class DepartmentList(generics.ListCreateAPIView):
-    queryset = Department.objects.all()
-    serializer_class = DepartmentSerializer
-    
-    permission_classes = [ permissions.IsAuthenticated ]
-
-
-class DepartmentDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Department.objects.all()
-    serializer_class = DepartmentSerializer
-    
-    permission_classes = [ permissions.IsAuthenticated ]
-    
-class EmployeeList(generics.ListCreateAPIView):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
-    
-    permission_classes = [ permissions.IsAuthenticated ]
-
-
-class EmployeeDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
-    
-    permission_classes = [ permissions.IsAuthenticated ]
-    
-class ClientList(generics.ListCreateAPIView):
-    queryset = Client.objects.all()
-    serializer_class = ClientSerializer
-    
-    permission_classes = [ permissions.IsAuthenticated ]
-
-
-class ClientDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Client.objects.all()
-    serializer_class = ClientSerializer
-    
-    permission_classes = [ permissions.IsAuthenticated ]
-    
-class GroupList(generics.ListCreateAPIView):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    
-    permission_classes = [ permissions.IsAuthenticated ]
-
-
-class GroupDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    
-    permission_classes = [ permissions.IsAuthenticated ]
-    
-class EventList(generics.ListCreateAPIView):
-    queryset = Event.objects.all()
-    serializer_class = EventSerializer
-    
-    permission_classes = [ permissions.IsAuthenticated ]
-
-
-class EventDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Event.objects.all()
-    serializer_class = EventSerializer
-    
-    permission_classes = [ permissions.IsAuthenticated ]
-    
-# Create your views here.
-# class CompanyViewSet(viewsets.ModelViewSet):
+# class CompanyList(generics.ListCreateAPIView):
 #     queryset = Company.objects.all()
-#     permission_classes = [
-#         permissions.IsAuthenticated
-#     ]
 #     serializer_class = CompanySerializer
+    
+#     permission_classes = [ permissions.IsAuthenticated ]
 
-#branch  
-# class BranchViewSet(viewsets.ModelViewSet):
+
+# class CompanyDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Company.objects.all()
+#     serializer_class = CompanySerializer
+    
+#     permission_classes = [ permissions.IsAuthenticated ]
+    
+# class BranchList(generics.ListCreateAPIView):
 #     queryset = Branch.objects.all()
-#     permission_classes = [
-#         permissions.IsAuthenticated
-#     ]
 #     serializer_class = BranchSerializer
+    
+#     permission_classes = [ permissions.IsAuthenticated ]
 
-# department  
-# class DepartmentViewSet(viewsets.ModelViewSet):
+
+# class BranchDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Branch.objects.all()
+#     serializer_class = BranchSerializer
+    
+#     permission_classes = [ permissions.IsAuthenticated ]
+    
+# class DepartmentList(generics.ListCreateAPIView):
 #     queryset = Department.objects.all()
-#     permission_classes = [
-#         permissions.IsAuthenticated
-#     ]
 #     serializer_class = DepartmentSerializer
+    
+#     permission_classes = [ permissions.IsAuthenticated ]
 
-# # employee   
-# class EmployeeViewSet(viewsets.ModelViewSet):
+
+# class DepartmentDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Department.objects.all()
+#     serializer_class = DepartmentSerializer
+    
+#     permission_classes = [ permissions.IsAuthenticated ]
+    
+# class EmployeeList(generics.ListCreateAPIView):
 #     queryset = Employee.objects.all()
-#     permission_classes = [
-#         permissions.IsAuthenticated
-#     ]
 #     serializer_class = EmployeeSerializer
     
-# # client   
-# class ClientViewSet(viewsets.ModelViewSet):
-#     queryset = Client.objects.all()
-#     permission_classes = [
-#         permissions.IsAuthenticated
-#     ]
-#     serializer_class = ClientSerializer
+#     permission_classes = [ permissions.IsAuthenticated ]
 
-# class UserViewSet(viewsets.ModelViewSet):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
+
+# class EmployeeDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Employee.objects.all()
+#     serializer_class = EmployeeSerializer
     
-#     @action(detail=True, methods=[ 'POST', 'PUT' ])
-#     def create(self, serializer):
-#         instance = serializer.save()
-#         instance.set_password(instance.password)
-#         instance.is_active = True
-#         instance.save()
+#     permission_classes = [ permissions.IsAuthenticated ]
+    
+# class ClientList(generics.ListCreateAPIView):
+#     queryset = Client.objects.all()
+#     serializer_class = ClientSerializer
+    
+#     permission_classes = [ permissions.IsAuthenticated ]
+
+
+# class ClientDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Client.objects.all()
+#     serializer_class = ClientSerializer
+    
+#     permission_classes = [ permissions.IsAuthenticated ]
+    
+# class GroupList(generics.ListCreateAPIView):
+#     queryset = Group.objects.all()
+#     serializer_class = GroupSerializer
+    
+#     permission_classes = [ permissions.IsAuthenticated ]
+
+
+# class GroupDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Group.objects.all()
+#     serializer_class = GroupSerializer
+    
+#     permission_classes = [ permissions.IsAuthenticated ]
+    
+# class EventList(generics.ListCreateAPIView):
+#     queryset = Event.objects.all()
+#     serializer_class = EventSerializer
+    
+#     permission_classes = [ permissions.IsAuthenticated ]
+
+
+# class EventDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Event.objects.all()
+#     serializer_class = EventSerializer
+    
+#     permission_classes = [ permissions.IsAuthenticated ]
+    
+# Create your views here.
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = Company.objects.all()
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+    serializer_class = CompanySerializer
+
+# branch  
+class BranchViewSet(viewsets.ModelViewSet):
+    queryset = Branch.objects.all()
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+    serializer_class = BranchSerializer
+
+# department  
+class DepartmentViewSet(viewsets.ModelViewSet):
+    queryset = Department.objects.all()
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+    serializer_class = DepartmentSerializer
+
+# employee   
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+    serializer_class = EmployeeSerializer
+    
+# client   
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+    serializer_class = ClientSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+    @action(detail=True, methods=[ 'POST', 'PUT' ])
+    def create(self, serializer):
+        instance = serializer.save()
+        instance.set_password(instance.password)
+        instance.is_active = True
+        instance.save()
         
 class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
@@ -178,18 +178,18 @@ class CreateAdminUser(generics.CreateAPIView):
         instance.save()
     
 # groups 
-# class GroupViewSet(viewsets.ModelViewSet):
-#     queryset = Group.objects.all()
-#     permission_classes = [
-#         permissions.IsAuthenticated
-#     ]
-#     serializer_class = GroupSerializer
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+    serializer_class = GroupSerializer
     
 # events    
-# class EventViewSet(viewsets.ModelViewSet):
-#     queryset = Event.objects.all()
-#     permission_classes = [
-#         permissions.IsAuthenticated
-#     ]
-#     serializer_class = EventSerializer
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+    serializer_class = EventSerializer
     
--- techleft/urls.py
+++ techleft/urls.py
@@ -21,48 +21,48 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-# from rest_framework import routers
+from rest_framework import routers
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
-# router = routers.DefaultRouter()
+router = routers.DefaultRouter()
 
-# router.register('api/companies', views.CompanyViewSet, basename='company')
-# router.register('api/branches', views.BranchViewSet, basename='branch')
-# router.register('api/departments', views.DepartmentViewSet, basename='department')
-# router.register('api/employees', views.EmployeeViewSet, basename='employee')
-# router.register('api/clients', views.ClientViewSet, basename='client')
-# router.register('api/groups', views.GroupViewSet, basename='group')
-# router.register('api/events', views.EventViewSet, basename='event')
+router.register('companies', views.CompanyViewSet, basename='company')
+router.register('branches', views.BranchViewSet, basename='branch')
+router.register('departments', views.DepartmentViewSet, basename='department')
+router.register('employees', views.EmployeeViewSet, basename='employee')
+router.register('clients', views.ClientViewSet, basename='client')
+router.register('groups', views.GroupViewSet, basename='group')
+router.register('events', views.EventViewSet, basename='event')
 
 urlpatterns = [
     # path('', include(router.urls)),
-    path('api/companies/', views.CompanyList.as_view(), name='company-list'),
-    path('api/companies/', views.CompanyDetail.as_view(), name='company-detail'),
+    # path('companies/', views.CompanyList.as_view(), name='company-list'),
+    # path('companies/', views.CompanyDetail.as_view(), name='company-detail'),
     
-    path('api/branches/', views.BranchList.as_view(), name='branch-list'),
-    path('api/branches/', views.BranchDetail.as_view(), name='branch-detail'),
+    # path('branches/', views.BranchList.as_view(), name='branch-list'),
+    # path('branches/', views.BranchDetail.as_view(), name='branch-detail'),
     
-    path('api/departments/', views.DepartmentList.as_view(), name='department-list'),
-    path('api/departments/', views.DepartmentDetail.as_view(), name='department-detail'),
+    # path('departments/', views.DepartmentList.as_view(), name='department-list'),
+    # path('departments/', views.DepartmentDetail.as_view(), name='department-detail'),
     
-    path('api/employees/', views.EmployeeList.as_view(), name='employee-list'),
-    path('api/employees/', views.EmployeeDetail.as_view(), name='employee-detail'),
+    # path('employees/', views.EmployeeList.as_view(), name='employee-list'),
+    # path('employees/', views.EmployeeDetail.as_view(), name='employee-detail'),
     
-    path('api/clients/', views.ClientList.as_view(), name='client-list'),
-    path('api/clients/', views.ClientDetail.as_view(), name='client-detail'),
+    # path('clients/', views.ClientList.as_view(), name='client-list'),
+    # path('clients/', views.ClientDetail.as_view(), name='client-detail'),
     
-    path('api/groups/', views.GroupList.as_view(), name='group-list'),
-    path('api/groups/', views.GroupDetail.as_view(), name='group-detail'),
+    # path('groups/', views.GroupList.as_view(), name='group-list'),
+    # path('groups/', views.GroupDetail.as_view(), name='group-detail'),
     
-    path('api/events/', views.EventList.as_view(), name='event-list'),
-    path('api/events/', views.EventDetail.as_view(), name='event-detail'),
+    # path('events/', views.EventList.as_view(), name='event-list'),
+    # path('events/', views.EventDetail.as_view(), name='event-detail'),
     
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
-    path('api/users/', views.UserList.as_view(), name='users-list'),
-    path('api/users/<int:pk>', views.UserDetail.as_view(), name='user-detail'),
-    path('api/admin/', views.CreateAdminUser.as_view(), name='admin-user'),
+    path('users/', views.UserList.as_view(), name='users-list'),
+    path('users/<int:pk>', views.UserDetail.as_view(), name='user-detail'),
+    path('admin/', views.CreateAdminUser.as_view(), name='admin-user'),
     
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
 
@@ -71,6 +71,6 @@ urlpatterns = [
     
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+# urlpatterns = format_suffix_patterns(urlpatterns)
 
-# urlpatterns += router.urls
+urlpatterns += router.urls
